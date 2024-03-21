781114812641c0825991f8bb8461d054dd33734f
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2024-03-21 15:01:21 +0800

    01

--- resources/views/any.blade.php
+++ resources/views/any.blade.php
@@ -6,6 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="description" content="馬尼馬尼Home 提供專業借據生成、支付命令指南，以及全面的本票裁定和法律諮詢服務。我們幫助您高效處理債務問題，確保合法權益。">
   <title>馬尼馬尼Home - 讓錢錢合法回到你口袋</title>
+  <link rel="icon" href="{{ asset('favicon.ico').'?'.rand() }}">
   <script src="{{ asset('js/app.js').'?'.rand() }}" defer></script>
   <link rel="stylesheet" href="{{ asset('css/app.css').'?'.rand() }}">
 </head>
